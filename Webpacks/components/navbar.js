85778b41727f9ccfe71e32b54e4f62792d856ef8
aakarsh604 <aakarsh604@gmail.com>
2022-05-01 01:44:10 +0530

    react assignment

---
+++ Webpacks/components/navbar.js
@@ -0,0 +1,15 @@
+const navbar = ( ) => {
+    return `   <div>
+    <div id="logo">FOOD APP</div>
+    <div><a href="index.html">Home</a></div>
+    <div><a href="search.html">Search</a></div>
+    <div><a href="receipeofDay.html">Receipe of the Day</a></div>
+</div>
+<div>
+    <div><a href="signup.html">SignUp</a></div>
+    <div><a href="login.html">LogIn</a></div>
+</div>`
+
+};
+
+export default navbar;
---
+++ Webpacks/src/index.js
@@ -0,0 +1,4 @@
+import navbar from "../components/navbar";
+import "../styles/navbar.css"
+
+document.querySelector("#navbar").innerHTML = navbar();
